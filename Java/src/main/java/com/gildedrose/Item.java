3679fcdf298c303a4191572b0aa77376be4f799d
Kurtis <90232216+Kurtain4774@users.noreply.github.com>
2022-10-06 20:11:56 -0600

    Week 6 Kattis Upload

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -13,9 +13,32 @@ public class Item {
         this.sellIn = sellIn;
         this.quality = quality;
     }
+    
+    public boolean validName() {
+    	if(name.length() > 0) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public boolean checkPriceLimits() {
+    	if(sellIn >= 0) {
+    		return true;
+    	}
+    	return false;
+    }
+    public boolean checkItemQualityLimit() {
+    	if(quality >= 0 && (quality <= 50 && !name.equals("Sulfuras"))) {
+    		return true;
+    	}
+    	return false;
+    }
+    
 
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+   
+   
 }
